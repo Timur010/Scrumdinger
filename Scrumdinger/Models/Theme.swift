e3c671a56d200968e7030d2c330ab7b582bd58a8
Timur010 <duakar010@gmail.com>
2023-03-04 13:39:20 +0300

    first commit

---
+++ Scrumdinger/Models/Theme.swift
@@ -0,0 +1,8 @@
+//
+//  Theme.swift
+//  Scrumdinger
+//
+//  Created by timur on 04.03.2023.
+//
+
+import Foundation
---
+++ Scrumdinger/TrailingIconLabelStyle.swift
@@ -0,0 +1,8 @@
+//
+//  TrailingIconLabelStyle.swift
+//  Scrumdinger
+//
+//  Created by timur on 04.03.2023.
+//
+
+import Foundation
